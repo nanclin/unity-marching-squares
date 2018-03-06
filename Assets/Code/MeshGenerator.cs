6587e2acc2ada7d3d1d05f8d10942642a0abc125
Nejc Anclin <anclin@MacBook-Pro-Anclin.local>
2018-03-06 22:21:36 +0100

    Generate grid from quads + Read sprites from atlas texture + Map sprites onto grid by marching squares id.

--- Assets/Code/MeshGenerator.cs
+++ Assets/Code/MeshGenerator.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class MeshGenerator : MonoBehaviour {
 
     public MeshFilter MeshFilter;
+    public Texture2D TileAtlas;
 
     private Vector3[] Vertices;
     private int[] Triangles;
     private Vector2[] UVs;
 
+    private Sprite[] TileSprites;
+
     void Start() {
-        GenerateGrid(3, 5);
+        GenerateGrid(3, 3);
     }
 
     private void GenerateGrid(int rows, int columns) {
 
-        int vertexCount = (columns + 1) * (rows + 1);
+        TileSprites = Resources.LoadAll<Sprite>(TileAtlas.name);
+
+        int vertexCount = columns * rows * 4;
         int trianglesCount = columns * rows * 2 * 3;
         Vertices = new Vector3[vertexCount];
         Triangles = new int[trianglesCount];
@@ -24,32 +29,65 @@ public class MeshGenerator : MonoBehaviour {
 
         int vertexIndex = 0;
         int triangleIndex = 0;
-        for (int r = 0; r < rows + 1; r++) {
-            for (int c = 0; c < columns + 1; c++) {
-                Vertices[vertexIndex] = new Vector3(c, r, 0);
-
-                float u = (float) c / (float) columns;
-                float v = (float) r / (float) rows;
-                UVs[vertexIndex] = new Vector2(u, v);
-
-                if (r < rows && c < columns) {
-                    Triangles[triangleIndex + 0] = vertexIndex;
-                    Triangles[triangleIndex + 1] = vertexIndex + columns + 1;
-                    Triangles[triangleIndex + 2] = vertexIndex + 1;
-                    triangleIndex += 3;
-
-                    Triangles[triangleIndex + 0] = vertexIndex + columns + 1;
-                    Triangles[triangleIndex + 1] = vertexIndex + columns + 2;
-                    Triangles[triangleIndex + 2] = vertexIndex + 1;
-                    triangleIndex += 3;
-                }
-
-                vertexIndex++;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++) {
+                AddMSTile(new Vector3(c, r), 1, 1, Random.Range(0, 16), ref vertexIndex, ref triangleIndex);
             }
         }
 
         MeshFilter.mesh.vertices = Vertices;
         MeshFilter.mesh.triangles = Triangles;
         MeshFilter.mesh.uv = UVs;
+    }
+
+    private void AddMSTile(Vector3 pos, float width, float height, int id, ref int vertexIndex, ref int triangleIndex) {
+
+        // vertices
+        Vertices[vertexIndex + 0] = pos;
+        Vertices[vertexIndex + 1] = pos + new Vector3(width, 0, 0);
+        Vertices[vertexIndex + 2] = pos + new Vector3(0, height, 0);
+        Vertices[vertexIndex + 3] = pos + new Vector3(width, height, 0);
+
+        // uvs
+        List<Vector2> uvs = GetMSTileUVs(id);
+        UVs[vertexIndex + 0] = uvs[0];
+        UVs[vertexIndex + 1] = uvs[1];
+        UVs[vertexIndex + 2] = uvs[2];
+        UVs[vertexIndex + 3] = uvs[3];
+
+        // triangles
+        Triangles[triangleIndex] = vertexIndex;
+        Triangles[triangleIndex + 1] = vertexIndex + 2;
+        Triangles[triangleIndex + 2] = vertexIndex + 1;
+        triangleIndex += 3;
+
+        Triangles[triangleIndex] = vertexIndex + 2;
+        Triangles[triangleIndex + 1] = vertexIndex + 3;
+        Triangles[triangleIndex + 2] = vertexIndex + 1;
+        triangleIndex += 3;
+
+        vertexIndex += 4;
+    }
+
+    private List<Vector2> GetMSTileUVs(int index) {
+        Rect rect = TileSprites[index].rect;
+
+        float x0 = rect.x;
+        float x1 = rect.x + rect.width;
+        float y0 = rect.y;
+        float y1 = rect.y + rect.height;
+
+        // convert to 0-1 range
+        float u0 = x0 / TileAtlas.width;
+        float u1 = x1 / TileAtlas.width;
+        float v0 = y0 / TileAtlas.height;
+        float v1 = y1 / TileAtlas.height;
+
+        return new List<Vector2>() {
+            new Vector2(u0, v0),
+            new Vector2(u1, v0),
+            new Vector2(u0, v1),
+            new Vector2(u1, v1),
+        };
     }
 }
