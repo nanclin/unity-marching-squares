06191878179be99b2d9d4b5293423b41fd7a44df
Nejc Anclin <anclin@MacBook-Pro-Anclin.local>
2018-03-27 20:32:23 +0200

    Sample corners of the grid.

--- Assets/Code/Grid.cs
+++ Assets/Code/Grid.cs
@@ -27,5 +27,24 @@ public class Grid : MonoBehaviour {
                 Handles.Label(pos, string.Format("{0}", Map[r, c]));
             }
         }
+
+        Gizmos.color = Color.white;
+        Handles.color = Color.white;
+        for (int r = 0; r < Height - 1; r++) {
+            for (int c = 0; c < Width - 1; c++) {
+                Vector3 pos = new Vector2(c, r) + Vector2.one * 0.5f;
+                Gizmos.DrawWireCube(pos, Vector3.one);
+
+                float treshold = 0.5f;
+                int c0 = (Map[r + 0, c + 0] >= treshold) ? 1 : 0;
+                int c1 = (Map[r + 0, c + 1] >= treshold) ? 2 : 0;
+                int c2 = (Map[r + 1, c + 0] >= treshold) ? 8 : 0;
+                int c3 = (Map[r + 1, c + 1] >= treshold) ? 4 : 0;
+                int id = c0 + c1 + c2 + c3;
+
+                //                Handles.Label(pos, string.Format("{0}+{1}+{2}+{3}={4}", c0, c1, c2, c3, id));
+                Handles.Label(pos, string.Format("{0}", id));
+            }
+        }
     }
 }
