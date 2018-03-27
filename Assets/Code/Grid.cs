1399cd17db2ff957c1f2b53d832517f082b62efc
Nejc Anclin <anclin@MacBook-Pro-Anclin.local>
2018-03-27 20:31:52 +0200

    Fix spawed x and y.

--- Assets/Code/Grid.cs
+++ Assets/Code/Grid.cs
@@ -3,8 +3,8 @@ using UnityEditor;
 
 public class Grid : MonoBehaviour {
 
-    private static int Width = 10;
-    private static int Height = 10;
+    private static int Width = 3;
+    private static int Height = 2;
     private int[,] Map = new int[Height, Width];
 
     // Use this for initialization
@@ -17,12 +17,14 @@ public class Grid : MonoBehaviour {
     }
 
     void OnDrawGizmos() {
-        for (int y = 0; y < Height; y++) {
-            for (int x = 0; x < Width; x++) {
-                Vector3 pos = new Vector2(y, x);
+        Gizmos.color = Color.gray;
+        Handles.color = Color.gray;
+        for (int r = 0; r < Height; r++) {
+            for (int c = 0; c < Width; c++) {
+                Vector3 pos = new Vector2(c, r);
                 Gizmos.DrawWireCube(pos, Vector3.one);
 //                Handles.Label(pos, string.Format("{0},{1}={2}", x, y, Map[y, x]));
-                Handles.Label(pos, string.Format("{0}", Map[y, x]));
+                Handles.Label(pos, string.Format("{0}", Map[r, c]));
             }
         }
     }
