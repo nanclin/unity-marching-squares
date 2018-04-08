ccbfd3cc84fd6de5c3905225e682a86629123917
Nejc Anclin <anclin@MacBook-Pro-Anclin.local>
2018-04-08 14:25:29 +0200

    Generate lerp marching squares mesh.

--- Assets/Code/MeshGeneratorLerpMS.cs
+++ Assets/Code/MeshGeneratorLerpMS.cs
@@ -22,6 +22,7 @@ public class MeshGeneratorLerpMS : MonoBehaviour {
         Vertices.Clear();
         Triangles.Clear();
         UVs.Clear();
+        MeshFilter.mesh.Clear();
 
         int rows = map.GetLength(0);
         int columns = map.GetLength(1);
@@ -32,11 +33,103 @@ public class MeshGeneratorLerpMS : MonoBehaviour {
                 // corner vertices
                 Vector2 v0 = new Vector2(c + 0, r + 0) + Vector2.one * 0.5f;
                 Vector2 v1 = new Vector2(c + 1, r + 0) + Vector2.one * 0.5f;
-                Vector2 v2 = new Vector2(c + 0, r + 1) + Vector2.one * 0.5f;
-                Vector2 v3 = new Vector2(c + 1, r + 1) + Vector2.one * 0.5f;
+                Vector2 v2 = new Vector2(c + 1, r + 1) + Vector2.one * 0.5f;
+                Vector2 v3 = new Vector2(c + 0, r + 1) + Vector2.one * 0.5f;
 
-                AddTriangle(v0, v2, v3);
-                AddTriangle(v0, v3, v1);
+                // corner values
+                float c0 = map[r + 0, c + 0];
+                float c1 = map[r + 0, c + 1];
+                float c2 = map[r + 1, c + 1];
+                float c3 = map[r + 1, c + 0];
+
+                int id =
+                    (c0 > treshold ? 1 : 0) * 1 +
+                    (c1 > treshold ? 1 : 0) * 2 +
+                    (c2 > treshold ? 1 : 0) * 4 +
+                    (c3 > treshold ? 1 : 0) * 8;
+
+                // remap from value to 0-1 range
+                float t0 = Mathf.InverseLerp(c0, c1, treshold);
+                float t1 = Mathf.InverseLerp(c1, c2, treshold);
+                float t2 = Mathf.InverseLerp(c2, c3, treshold);
+                float t3 = Mathf.InverseLerp(c3, c0, treshold);
+                // get edge vertex
+                Vector2 eB = Vector2.Lerp(v0, v1, t0);
+                Vector2 eR = Vector2.Lerp(v1, v2, t1);
+                Vector2 eT = Vector2.Lerp(v2, v3, t2);
+                Vector2 eL = Vector2.Lerp(v3, v0, t3);
+
+                switch (id) {
+                // 1 triangle
+                    case 1:
+                        AddTriangle(v0, eL, eB);
+                        break;
+                    case 2:
+                        AddTriangle(eB, eR, v1);
+                        break;
+                    case 4:
+                        AddTriangle(eT, v2, eR);
+                        break;
+                    case 8:
+                        AddTriangle(v3, eT, eL);
+                        break;
+                    // 2 triangles
+                        break;
+                    case 3:
+                        AddTriangle(v0, eL, v1);
+                        AddTriangle(eL, eR, v1);
+                        break;
+                    case 6:
+                        AddTriangle(eT, v2, v1);
+                        AddTriangle(eT, v1, eB);
+                        break;
+                    case 9:
+                        AddTriangle(v3, eT, eB);
+                        AddTriangle(v3, eB, v0);
+                        break;
+                    case 12:
+                        AddTriangle(v3, v2, eR);
+                        AddTriangle(v3, eR, eL);
+                        break;
+                    case 15:
+                        AddTriangle(v0, v3, v2);
+                        AddTriangle(v0, v2, v1);
+                        break;
+                    // 3 triangles
+                    case 7:
+                        AddTriangle(v0, eL, eT);
+                        AddTriangle(v0, eT, v2);
+                        AddTriangle(v0, v2, v1);
+                        break;
+                    case 11:
+                        AddTriangle(v0, v3, eT);
+                        AddTriangle(v0, eT, eR);
+                        AddTriangle(v0, eR, v1);
+                        break;
+                    case 13:
+                        AddTriangle(v0, v3, v2);
+                        AddTriangle(v0, v2, eR);
+                        AddTriangle(v0, eR, eB);
+                        break;
+                    case 14:
+                        AddTriangle(eL, v3, v2);
+                        AddTriangle(eL, v2, v1);
+                        AddTriangle(eL, v1, eB);
+                        break;
+                    // 4 triangles
+                    case 5:
+                        AddTriangle(v0, eL, eT);
+                        AddTriangle(v0, eT, v2);
+                        AddTriangle(v0, v2, eR);
+                        AddTriangle(v0, eR, eB);
+                        break;
+                    case 10:
+                        AddTriangle(eL, v3, eT);
+                        AddTriangle(eL, eT, eR);
+                        AddTriangle(eL, eR, v1);
+                        AddTriangle(eL, v1, eB);
+                        break;
+                }
             }
         }
 
